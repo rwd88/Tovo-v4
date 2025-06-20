fb0a0a2d440864c3c767cdbe63f3736f011ee8f4
Youssef Medlej <youssefmedlej11@gmail.com>
2025-06-20 16:10:45 +0300

    Add console.log to ConnectWalletButton for click debugging

--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -1,14 +1,29 @@
 // components/ConnectWalletButton.tsx
+import React from 'react'
 import { useEthereum } from '../contexts/EthereumContext'
 
 export default function ConnectWalletButton() {
   const { address, connect, disconnect } = useEthereum()
-  return address ? (
-    <div>
-      Connected: {address.substring(0, 6)}…{address.slice(-4)}
-      <button onClick={disconnect}>Disconnect</button>
-    </div>
-  ) : (
-    <button onClick={connect}>Connect MetaMask</button>
+
+  if (address) {
+    return (
+      <div>
+        Connected: {address.substring(0, 6)}…{address.slice(-4)}
+        <button onClick={() => { console.log('disconnect clicked'); disconnect() }}>
+          Disconnect
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <button
+      onClick={() => {
+        console.log('connect clicked')
+        connect()
+      }}
+    >
+      Connect MetaMask
+    </button>
   )
 }
