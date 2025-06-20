c8e23bcc42fa5ec4ac6881dac0065170bd569fa3
Youssef Medlej <youssefmedlej11@gmail.com>
2025-06-20 15:59:34 +0300

    Fix import paths for EthereumContext to use relative paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 // pages/_app.tsx
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { EthereumProvider } from '@/contexts/EthereumContext'
+import { EthereumProvider } from '../contexts/EthereumContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
