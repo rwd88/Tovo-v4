969a42dd214d407dd17fbfcea3ec5996ff079931
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-07 11:39:49 +0300

    fix(ui): use TonConnectUIProvider from @tonconnect/ui-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-// EVM context (your existing)
+// EVM context
 import { EthereumProvider } from '../contexts/EthereumContext'
 
 // Solana Wallet Adapter
@@ -18,30 +18,30 @@ import {
 } from '@solana/wallet-adapter-wallets'
 
 // TON Connect UI
-import { TonConnectProvider } from '@tonconnect/ui-react'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-// Your NavBar with all three connect buttons
+// Your NavBar
 import NavBar from '../src/components/NavBar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  // Solana setup (you already had this)
+  // Solana setup
   const solanaNetwork  = WalletAdapterNetwork.Mainnet
   const solanaEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL!
   const solanaWallets  = [ new PhantomWalletAdapter(), new SolflareWalletAdapter() ]
 
   return (
-    <TonConnectProvider>
+    <TonConnectUIProvider>
       <EthereumProvider>
         <ConnectionProvider endpoint={solanaEndpoint}>
           <WalletProvider wallets={solanaWallets} autoConnect>
-            {/* Now all providers are live before NavBar */}
+
+            {/* NavBar now inside all providers */}
             <NavBar />
 
-            {/* And then your page content */}
             <Component {...pageProps} />
           </WalletProvider>
         </ConnectionProvider>
       </EthereumProvider>
-    </TonConnectProvider>
+    </TonConnectUIProvider>
   )
 }
