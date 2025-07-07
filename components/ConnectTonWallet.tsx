c52b717490cfcab694dbc9194a6881ae64d3ef88
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-07 16:11:02 +0300

    Connected TON wallet and showing address

--- components/ConnectTonWallet.tsx
+++ components/ConnectTonWallet.tsx
@@ -1,11 +1,14 @@
-// components/ConnectTonWallet.tsx
 "use client"
-import { TonConnectButton } from "@tonconnect/ui-react"
+import { TonConnectButton, useTonConnectUI } from "@tonconnect/ui-react"
 
 export default function ConnectTonWallet() {
+  const [tonConnectUI] = useTonConnectUI()
+  const connected = tonConnectUI.account?.address
+
   return (
     <div className="mb-4">
       <TonConnectButton />
+      {connected && <p className="mt-2 text-white">TON Address: {connected}</p>}
     </div>
   )
 }
