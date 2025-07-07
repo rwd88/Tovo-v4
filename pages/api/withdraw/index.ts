b584f89a98751ef8c894e913e0e14c5a5efa8e07
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-07 15:22:16 +0300

    fix: update withdrawal API to use correct Prisma import

--- pages/api/withdraw/index.ts
+++ pages/api/withdraw/index.ts
@@ -1,6 +1,6 @@
 // pages/api/withdraw/index.ts
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../lib/prisma'
+import { prisma } from '../../../lib/prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // you can scope to the logged-in user, or return all for an admin dashboard
