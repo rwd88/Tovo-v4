d01557de155d3e4cd7fbddaf2f88dd1a1e604bcd
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-07 11:37:45 +0300

    fix(ui): wrap NavBar in Solana & TON providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,40 +2,46 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-// Global Providers
+// EVM context (your existing)
 import { EthereumProvider } from '../contexts/EthereumContext'
-import NavBar from '../src/components/NavBar'
 
-// Solana Wallet Adapter styles & providers
+// Solana Wallet Adapter
 import '@solana/wallet-adapter-react-ui/styles.css'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import {
   ConnectionProvider,
-  WalletProvider,
+  WalletProvider
 } from '@solana/wallet-adapter-react'
 import {
   PhantomWalletAdapter,
-  SolflareWalletAdapter,
+  SolflareWalletAdapter
 } from '@solana/wallet-adapter-wallets'
 
-// Environment variables
-const solanaNetwork = WalletAdapterNetwork.Mainnet
-const solanaEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL!
-const solanaWallets = [
-  new PhantomWalletAdapter(),
-  new SolflareWalletAdapter(),
-]
+// TON Connect UI
+import { TonConnectProvider } from '@tonconnect/ui-react'
+
+// Your NavBar with all three connect buttons
+import NavBar from '../src/components/NavBar'
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Solana setup (you already had this)
+  const solanaNetwork  = WalletAdapterNetwork.Mainnet
+  const solanaEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL!
+  const solanaWallets  = [ new PhantomWalletAdapter(), new SolflareWalletAdapter() ]
+
   return (
-    <EthereumProvider>
-      {/* Wrap NavBar within wallet providers so all connectors have context */}
-      <ConnectionProvider endpoint={solanaEndpoint}>
-        <WalletProvider wallets={solanaWallets} autoConnect>
-          <NavBar />
-          <Component {...pageProps} />
-        </WalletProvider>
-      </ConnectionProvider>
-    </EthereumProvider>
+    <TonConnectProvider>
+      <EthereumProvider>
+        <ConnectionProvider endpoint={solanaEndpoint}>
+          <WalletProvider wallets={solanaWallets} autoConnect>
+            {/* Now all providers are live before NavBar */}
+            <NavBar />
+
+            {/* And then your page content */}
+            <Component {...pageProps} />
+          </WalletProvider>
+        </ConnectionProvider>
+      </EthereumProvider>
+    </TonConnectProvider>
   )
 }
