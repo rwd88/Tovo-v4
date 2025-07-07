c14f68ed174188142099fb9869eb18958101b9f6
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-07 11:00:26 +0300

    feat(ui): include NavBar in App layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,41 @@
 // pages/_app.tsx
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { EthereumProvider } from '../contexts/EthereumContext';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+// Wallet & Context Providers
+import { EthereumProvider } from '../contexts/EthereumContext'
+import NavBar from '../src/components/NavBar'
 
 // Solana Wallet Adapter styles & providers
-import '@solana/wallet-adapter-react-ui/styles.css';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import '@solana/wallet-adapter-react-ui/styles.css'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import {
   ConnectionProvider,
   WalletProvider,
-} from '@solana/wallet-adapter-react';
+} from '@solana/wallet-adapter-react'
 import {
   PhantomWalletAdapter,
   SolflareWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-
+} from '@solana/wallet-adapter-wallets'
 
 // Environment variables
-const solanaNetwork = WalletAdapterNetwork.Mainnet;
-const solanaEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL!;
+const solanaNetwork = WalletAdapterNetwork.Mainnet
+const solanaEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL!
 const solanaWallets = [
   new PhantomWalletAdapter(),
   new SolflareWalletAdapter(),
-];
+]
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <EthereumProvider>
+      <NavBar />
+
       <ConnectionProvider endpoint={solanaEndpoint}>
         <WalletProvider wallets={solanaWallets} autoConnect>
           <Component {...pageProps} />
         </WalletProvider>
       </ConnectionProvider>
     </EthereumProvider>
-  );
+  )
 }
