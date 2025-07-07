8570687aee840ea50de2100ccf16b7e808f8c6a7
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-07 13:27:30 +0300

    fix(ui): dynamically import NavBar to avoid SSR window errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,50 +3,42 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
 
-// EVM context
+// Global Ethereum context provider
 import { EthereumProvider } from '../contexts/EthereumContext'
 
-// Solana Wallet Adapter
+// Dynamically import NavBar on client side to avoid SSR "window" errors
+const NavBar = dynamic(() => import('../src/components/NavBar'), { ssr: false })
+
+// Solana Wallet Adapter styles & providers
 import '@solana/wallet-adapter-react-ui/styles.css'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import {
   ConnectionProvider,
-  WalletProvider
+  WalletProvider,
 } from '@solana/wallet-adapter-react'
 import {
   PhantomWalletAdapter,
-  SolflareWalletAdapter
+  SolflareWalletAdapter,
 } from '@solana/wallet-adapter-wallets'
 
-// TON Connect UI
-import { TonConnectUIProvider } from '@tonconnect/ui-react'
-
-// Dynamically load NavBar only on client
-const NavBar = dynamic(() => import('../src/components/NavBar'), {
-  ssr: false
-})
+// Environment variables
+const solanaNetwork = WalletAdapterNetwork.Mainnet
+const solanaEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL!
+const solanaWallets = [
+  new PhantomWalletAdapter(),
+  new SolflareWalletAdapter(),
+]
 
 export default function App({ Component, pageProps }: AppProps) {
-  // Solana config
-  const solanaEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL!
-  const solanaWallets  = [
-    new PhantomWalletAdapter(),
-    new SolflareWalletAdapter()
-  ]
-
   return (
-    <TonConnectUIProvider>
-      <EthereumProvider>
-        <ConnectionProvider endpoint={solanaEndpoint}>
-          <WalletProvider wallets={solanaWallets} autoConnect>
-            {/* Only render NavBar on the client */}
-            <NavBar />
-
-            {/* Rest of your app */}
-            <Component {...pageProps} />
-          </WalletProvider>
-        </ConnectionProvider>
-      </EthereumProvider>
-    </TonConnectUIProvider>
+    <EthereumProvider>
+      {/* Wrap NavBar within wallet providers */}
+      <ConnectionProvider endpoint={solanaEndpoint}>
+        <WalletProvider wallets={solanaWallets} autoConnect>
+          <NavBar />
+          <Component {...pageProps} />
+        </WalletProvider>
+      </ConnectionProvider>
+    </EthereumProvider>
   )
 }
