073a785be9776f314289f762039aeac95184539c
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-10 17:00:23 +0300

    markets

--- pages/api/markets.ts
+++ pages/api/markets.ts
@@ -1,6 +1,6 @@
 // pages/api/markets/active.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '../../../lib/prisma';
+import { prisma } from '../../lib/prisma';
 
 export default async function handler(
   req: NextApiRequest,
