cb434f49c26eaa38d47b34b1f7399e3d220f0d89
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-24 15:08:05 +0300

    abc

--- components/Web3Providers.tsx
+++ components/Web3Providers.tsx
@@ -1,26 +1,16 @@
+// components/Web3Providers.tsx
 'use client'
 
-import React, { ReactNode, useEffect } from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-// your three contexts
 import { EthereumProvider } from '../contexts/EthereumContext'
-import { SolanaProvider }   from '../contexts/SolanaContext'
-import { TonProvider }      from '../contexts/TonContext'
+import { SolanaProvider } from '../contexts/SolanaContext'
+import { TonProvider } from '../contexts/TonContext'
 
-// Solana UI wrapper (inside your SolanaProvider you already use WalletAdapter & Modal)
-import '@solana/wallet-adapter-react-ui/styles.css'
-
-export default function Web3Providers({ children }: { children: ReactNode }) {
-  useEffect(() => console.log('🔌 Web3Providers mounted'), [])
-
+export const Web3Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <EthereumProvider>
       <SolanaProvider>
         <TonProvider>
-          <QueryClientProvider client={new QueryClient()}>
-            {children}
-          </QueryClientProvider>
+          {children}
         </TonProvider>
       </SolanaProvider>
     </EthereumProvider>
