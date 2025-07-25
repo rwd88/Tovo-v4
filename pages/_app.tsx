dd6ee811fedf6a357b1833a29dd9206fc6510d71
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-25 15:39:28 +0300

    abc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,40 @@
-// pages/_app.tsx
-'use client'
-
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import dynamic from 'next/dynamic'
 import { ErrorBoundary } from '../components/ErrorBoundary'
 
-const Web3Providers = dynamic(
-  () => import('../components/Web3Providers').then((mod) => mod.Web3Providers),
-  { 
-    ssr: false,
-    loading: () => <div className="loading">Loading Web3...</div>
-  }
+// Wagmi & RainbowKit imports
+import { WagmiConfig, createClient, configureChains, chain } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
+
+// Set up chains and providers
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [publicProvider()]
 )
+
+// Get default connectors (MetaMask, WalletConnect, etc.)
+const { connectors } = getDefaultWallets({
+  appName: 'Tovo Prediction',
+  chains,
+})
+
+// Create Wagmi client
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
-      <Web3Providers>
-        <Component {...pageProps} />
-      </Web3Providers>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={darkTheme()}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ErrorBoundary>
   )
 }
