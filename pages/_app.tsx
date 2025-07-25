3cb81f8b02ba3e7104b3567d272164ca4cf5747c
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-25 15:52:08 +0300

    abc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ErrorBoundary } from '../components/ErrorBoundary'
 
-// Wagmi v2 + TanStack Query
+// Wagmi v2 + TanStack Query (EVM)
 import { WagmiProvider, createConfig } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
 import { http } from 'viem'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Solana wallet adapter
+import { ConnectionProvider, WalletProvider as SolanaWalletProvider } from '@solana/wallet-adapter-react'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import '@solana/wallet-adapter-react-ui/styles.css'
 
 // TON Connect UI
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
@@ -14,25 +21,37 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 // Setup TanStack Query
 const queryClient = new QueryClient()
 
-// Configure Wagmi v2
-const config = createConfig({
+// Configure Wagmi v2 for EVM
+const evmConfig = createConfig({
   autoConnect: true,
   publicClient: http(process.env.NEXT_PUBLIC_ETH_RPC_URL!),
   chains: [mainnet],
 })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  // Solana network and wallets
+  const solanaNetwork = WalletAdapterNetwork.Mainnet
+  const solanaEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_URL!
+  const solanaWallets = [new PhantomWalletAdapter()]
+
   return (
     <ErrorBoundary>
-      {/* TON Connect wrapper: provide manifest URL for TonConnect */}
+      {/* TON Connect wrapper */}
       <TonConnectUIProvider manifestUrl={process.env.NEXT_PUBLIC_TON_MANIFEST_URL!}>
-        {/* React Query provider */}
-        <QueryClientProvider client={queryClient}>
-          {/* Wagmi (EVM) provider */}
-          <WagmiProvider config={config}>
-            <Component {...pageProps} />
-          </WagmiProvider>
-        </QueryClientProvider>
+        {/* Solana wallet adapter provider */}
+        <ConnectionProvider endpoint={solanaEndpoint}>
+          <SolanaWalletProvider wallets={solanaWallets} autoConnect>
+            <WalletModalProvider>
+              {/* React Query provider */}
+              <QueryClientProvider client={queryClient}>
+                {/* Wagmi (EVM) provider */}
+                <WagmiProvider config={evmConfig}>
+                  <Component {...pageProps} />
+                </WagmiProvider>
+              </QueryClientProvider>
+            </WalletModalProvider>
+          </SolanaWalletProvider>
+        </ConnectionProvider>
       </TonConnectUIProvider>
     </ErrorBoundary>
   )
