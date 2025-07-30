968b2a33c360f99a6eade8b3092c48bdbff5b5a7
Youssef Medlej <youssefmedlej11@gmail.com>
2025-07-30 12:35:51 +0300

    abc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang="en" className="light">
+      <Head>
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <body style={{ background: 'white', color: '#000' }}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
