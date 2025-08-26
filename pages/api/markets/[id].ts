13f94342abf17e7c660e43d2032e982447bcffee
Youssef Medlej <youssefmedlej11@gmail.com>
2025-08-26 11:20:02 +0300

    aaa

--- pages/api/markets/[id].ts
+++ pages/api/markets/[id].ts
@@ -1,6 +1,6 @@
 // pages/api/markets/[id].ts
 import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '@/lib/prisma'
+import prisma from '../../../lib/prisma'
 
 export const runtime = 'nodejs'
 
